e0a39ddffc0a3d24c9fb226a605e813107d822ee
bertrandmandrin <bertrandmandrin@7bd58650-3e41-0410-a3f2-b94294cb9d76>
2008-02-29 14:34:57 +0000

    Bug correction : time starts when N/P are clicked + REINDENTATION

--- src/src/com/sibyl/ui/PlayerUI.java
+++ src/src/com/sibyl/ui/PlayerUI.java
@@ -50,7 +50,8 @@ import com.sibyl.MusicDB;
 import com.sibyl.R;
 import com.sibyl.Sibylservice;
 
-public class PlayerUI extends Activity {
+public class PlayerUI extends Activity 
+{
 
     ISibylservice mService = null;
 
@@ -78,194 +79,206 @@ public class PlayerUI extends Activity {
     // time elapsed when playing a song
     private int time;
     private Handler mHandler = new Handler();
-    private Runnable timerTask = new Runnable() {
-	public void run() {
-	    // re adjusting time
-	    if( time%10 == 0){
-		try{
-		    time = mService.getCurrentPosition()/1000;
-		}catch(DeadObjectException ex){}
-	    }
-	    // display time
-	    elapsedTime.setText(DateUtils.formatElapsedTime(time));
-	    time++;
-	    // re update timer in 1 sec
-	    mHandler.postDelayed(this, 1000);
-	}
+    private Runnable timerTask = new Runnable() 
+    {
+        public void run() 
+        {
+            // re adjusting time
+            if( time%10 == 0)
+            {
+                try
+                {
+                    time = mService.getCurrentPosition()/1000;
+                }
+                catch(DeadObjectException ex){}
+            }
+            // display time
+            elapsedTime.setText(DateUtils.formatElapsedTime(time));
+            time++;
+            // re update timer in 1 sec
+            mHandler.postDelayed(this, 1000);
+        }
     };
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle icicle) {
-	super.onCreate(icicle);
-	setContentView(R.layout.main);
-	//launch the service.
-	launchService();
-	//get the views
-	artiste = (TextView) findViewById(R.id.artiste);
-	titre = (TextView) findViewById(R.id.titre);
-	elapsedTime = (TextView) findViewById(R.id.tpsEcoule);
-	tempsTotal = (TextView) findViewById(R.id.tpsTotal);
-	lecture = (Button) findViewById(R.id.lecture);
-	lecture.setOnClickListener(mPlayListener);
-	next = (Button) findViewById(R.id.next);
-	previous = (Button) findViewById(R.id.prec);
-	//set cover
-	ImageView cover = (ImageView) findViewById(R.id.cover);
-	cover.setImageDrawable(Drawable.createFromPath("/data/music/cover.jpg"));  
-	//set listenner
-	lecture.setOnClickListener(mPlayListener);
-	next.setOnClickListener(mNextListener);
-	previous.setOnClickListener(mPreviousListener);
+    public void onCreate(Bundle icicle) 
+    {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+        //launch the service.
+        launchService();
+        //get the views
+        artiste = (TextView) findViewById(R.id.artiste);
+        titre = (TextView) findViewById(R.id.titre);
+        elapsedTime = (TextView) findViewById(R.id.tpsEcoule);
+        tempsTotal = (TextView) findViewById(R.id.tpsTotal);
+        lecture = (Button) findViewById(R.id.lecture);
+        lecture.setOnClickListener(mPlayListener);
+        next = (Button) findViewById(R.id.next);
+        previous = (Button) findViewById(R.id.prec);
+        //set cover
+        ImageView cover = (ImageView) findViewById(R.id.cover);
+        cover.setImageDrawable(Drawable.createFromPath("/data/music/cover.jpg"));  
+        //set listenner
+        lecture.setOnClickListener(mPlayListener);
+        next.setOnClickListener(mNextListener);
+        previous.setOnClickListener(mPreviousListener);
 
-	
-
-	//create or connect to the Database
-	try{
-	    mdb = new MusicDB(this);
-	    Log.v(TAG,"BD OK");
-	}catch(Exception ex){
-	    Log.v(TAG, ex.toString()+" Create");
-	}
+        //create or connect to the Database
+        try
+        {
+            mdb = new MusicDB(this);
+            Log.v(TAG,"BD OK");
+        }
+        catch(Exception ex)
+        {
+            Log.v(TAG, ex.toString()+" Create");
+        }
     }
     
     
     
 
     @Override
-    protected void onDestroy() {
-	super.onDestroy();
-	mHandler.removeCallbacks(timerTask);
+    protected void onDestroy() 
+    {
+        super.onDestroy();
+        mHandler.removeCallbacks(timerTask);
     }
 
-    public void launchService()	{
-	Bundle args = new Bundle();
-	args.putString("filename", "test.mp3");
-	bindService(new Intent(PlayerUI.this,
+    public void launchService()	
+    {
+        Bundle args = new Bundle();
+        args.putString("filename", "test.mp3");
+        bindService(new Intent(PlayerUI.this,
 		Sibylservice.class), mConnection, Context.BIND_AUTO_CREATE);
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	super.onCreateOptionsMenu(menu);
-	menu.add(0, QUIT_ID, R.string.menu_quit);
-	menu.add(0, PLAYLIST_ID, R.string.menu_playList);
-	menu.add(0, OPTION_ID, R.string.menu_option);
-	menu.add(0,ADD_ID, "Add "+Music.MUSIC_DIR);
-	return true;
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, QUIT_ID, R.string.menu_quit);
+        menu.add(0, PLAYLIST_ID, R.string.menu_playList);
+        menu.add(0, OPTION_ID, R.string.menu_option);
+        menu.add(0,ADD_ID, "Add "+Music.MUSIC_DIR);
+        return true;
     }
 
     @Override
-    public boolean onMenuItemSelected(int featureId, Item item) {
-	super.onMenuItemSelected(featureId, item);
-	switch(item.getId()) {
-	case QUIT_ID:
-	    try {
-		mService.stop();
-	    }//lors de l'appel de fonction de interface (fichier aidl)
-	    //il faut catcher les DeadObjectException
-	    catch (DeadObjectException ex) {
-	    }
-	    play = false;
-	    pause = false;
-	    lecture.setText(R.string.play);
-        finish();
-	    break;
-	case PLAYLIST_ID:
-	    //launch the playlist's activity
-	    break;
-	case OPTION_ID:
-	    //launch the option's activity
-	    break;
-	case ADD_ID:
-	    //add song
-	    fillBD(Music.MUSIC_DIR+"/");
-	    fillPlayList();
-	    break;
-	}
-
-	return true;
+    public boolean onMenuItemSelected(int featureId, Item item) 
+    {
+        super.onMenuItemSelected(featureId, item);
+        switch(item.getId()) 
+        {
+        case QUIT_ID:
+            try 
+            {
+                mService.stop();
+            }//lors de l'appel de fonction de interface (fichier aidl)
+            //il faut catcher les DeadObjectException
+            catch (DeadObjectException ex) {}
+            play = false;
+            pause = false;
+            lecture.setText(R.string.play);
+            finish();
+            break;
+        case PLAYLIST_ID:
+            //launch the playlist's activity
+            break;
+        case OPTION_ID:
+            //launch the option's activity
+            break;
+        case ADD_ID:
+            //add song
+            fillBD(Music.MUSIC_DIR+"/");
+            fillPlayList();
+            break;
+        }
+        
+        return true;
     }
 
-
-
+    
     private ServiceConnection mConnection = new ServiceConnection()
     {
-	public void onServiceConnected(ComponentName className, IBinder service)
-	{
-	    // This is called when the connection with the service has been
-	    // established, giving us the service object we can use to
-	    // interact with the service.  We are communicating with our
-	    // service through an IDL interface, so get a client-side
-	    // representation of that from the raw service object.
-	    mService = ISibylservice.Stub.asInterface((IBinder)service);
+        public void onServiceConnected(ComponentName className, IBinder service)
+        {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  We are communicating with our
+            // service through an IDL interface, so get a client-side
+            // representation of that from the raw service object.
+            mService = ISibylservice.Stub.asInterface((IBinder)service);
 
-	    //remplacant du NotificationManager/notifyWithText
-	    Toast.makeText(PlayerUI.this, "Connexion au service reussie", 
-		    Toast.LENGTH_SHORT).show();
+            //remplacant du NotificationManager/notifyWithText
+            Toast.makeText(PlayerUI.this, "Connexion au service reussie", 
+                    Toast.LENGTH_SHORT).show();
 
-	}
+        }
 
-	public void onServiceDisconnected(ComponentName className)
-	{
-	    // This is called when the connection with the service has been
-	    // unexpectedly disconnected -- that is, its process crashed.
-	    mService = null;
+        public void onServiceDisconnected(ComponentName className)
+        {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
 
-	    //remplacant du NotificationManager/notifyWithText
-	    Toast.makeText(PlayerUI.this, "Deconnexion du service", 
-		    Toast.LENGTH_SHORT).show();                              
-	}
+            //remplacant du NotificationManager/notifyWithText
+            Toast.makeText(PlayerUI.this, "Deconnexion du service", 
+                    Toast.LENGTH_SHORT).show();                              
+        }
     };
     
     //set the total time of the song which is played
     private void setTotalTime ()
     {
-	    try {
-			tempsTotal.setText(DateUtils.formatElapsedTime(mService.getDuration()/1000));
-		    }catch (DeadObjectException ex){}
+        try 
+        {
+            tempsTotal.setText(DateUtils.formatElapsedTime(mService.getDuration()/1000));
+        }
+        catch (DeadObjectException ex){}
     }
 
     private OnClickListener mPlayListener = new OnClickListener()
     {
-	public void onClick(View v)
-	{
-	    if( play) //call if a music is played (pause the music)
-	    {
-			lecture.setText(R.string.play);
-			try {
-			    mService.pause();
-			}//lors de l'appel de fonction de interface (fichier aidl)
-			//il faut catcher les DeadObjectException
-			catch (DeadObjectException ex) {
-			}
-			pause = true;
-			// remove timer task from ui thread
-			mHandler.removeCallbacks(timerTask);
-	    }
-	    else // to start listening a music or resume.
-	    {
-			lecture.setText(R.string.pause);
-			try {
-			    mService.start();
-			}//lors de l'appel de fonction de interface (fichier aidl)
-			//il faut catcher les DeadObjectException
-			catch (DeadObjectException ex) {
-			}
-			if(!pause) //a song is start.
-			{
-				setTotalTime();
-			}
-			// reset timer so it will be recalculated if resuming
-			time = 0;
-			mHandler.removeCallbacks(timerTask);
-			// add timer task to ui thread
-			mHandler.post(timerTask);
-			pause = false;
-	    }
-	    play = !play;
-	}
+        public void onClick(View v)
+        {
+            if( play) //call if a music is played (pause the music)
+            {
+                lecture.setText(R.string.play);
+                try 
+                {
+                    mService.pause();
+                }//lors de l'appel de fonction de interface (fichier aidl)
+                //il faut catcher les DeadObjectException
+                catch (DeadObjectException ex) {}
+                pause = true;
+                // remove timer task from ui thread
+                mHandler.removeCallbacks(timerTask);
+            }
+            else // to start listening a music or resume.
+            {
+                lecture.setText(R.string.pause);
+                try 
+                {
+                    mService.start();
+                }//lors de l'appel de fonction de interface (fichier aidl)
+                //il faut catcher les DeadObjectException
+                catch (DeadObjectException ex) {}
+                if(!pause) //a song is start.
+                {
+                    setTotalTime();
+                }
+                // reset timer so it will be recalculated if resuming
+                time = 0;
+                mHandler.removeCallbacks(timerTask);
+                // add timer task to ui thread
+                mHandler.post(timerTask);
+                pause = false;
+            }
+            play = !play;
+        }
     };
     
     
@@ -276,7 +289,10 @@ public class PlayerUI extends Activity {
         {
             try
             {
+                mHandler.removeCallbacks(timerTask);
                 time = 0;
+                // add timer task to ui thread
+                mHandler.post(timerTask);
                 mService.next();
                 //is a song really played ???
                 setTotalTime();
@@ -289,78 +305,82 @@ public class PlayerUI extends Activity {
     };
     
 	//Listenner for the button previous. Play the previous sont in the playlist
-	private OnClickListener mPreviousListener = new OnClickListener()
+    private OnClickListener mPreviousListener = new OnClickListener()
     {
-        public void onClick(View v)
-		{
-		    try
-            {
+	    public void onClick(View v)
+	    {
+	        try
+	        {
+                mHandler.removeCallbacks(timerTask);
                 time = 0;
-                mService.prev();
-                //is a song really played ???
-                setTotalTime();
-                play = true;
-                pause = false;
-                lecture.setText(R.string.pause);
-            }
-		    catch (DeadObjectException ex){}
-		}
+                // add timer task to ui thread
+                mHandler.post(timerTask);
+	            mService.prev();
+	            //is a song really played ???
+	            setTotalTime();
+	            play = true;
+	            pause = false;
+	            lecture.setText(R.string.pause);
+	        }
+	        catch (DeadObjectException ex){}
+	    }
     };
     
     
     private void fillBD (String path)
     {
-	// get all mp3 files in path
-	try
-	{
-	    File dir = new File(path);
-	    Log.v(TAG, "Insert");
-	    FilenameFilter filter = new FilenameFilter() 
-	    {
-		public boolean accept(File dir, String name) 
-		{
-		    return name.endsWith(".mp3");
-		}
-	    };
+        // get all mp3 files in path
+        try
+        {
+            File dir = new File(path);
+            Log.v(TAG, "Insert");
+            FilenameFilter filter = new FilenameFilter() 
+            {
+                public boolean accept(File dir, String name) 
+                {
+                    return name.endsWith(".mp3");
+                }
+            };
 
-	    // insert them in the database    
-	    for(String s : dir.list(filter))
-	    {
-		try
-		{
-		    long t = System.currentTimeMillis();
-		    mdb.insert(path+s);
-		    Log.v(TAG, "temps "+(System.currentTimeMillis()-t));
-		}
-		catch(SQLiteException sqle)
-		{
-		    Log.v(TAG, "sql" + sqle.toString());
-		}
-	    }
-	}
-	catch(Exception ex)
-	{
-	    Log.v(TAG, ex.toString());
-	}
+            // insert them in the database    
+            for(String s : dir.list(filter))
+            {
+                try
+                {
+                    long t = System.currentTimeMillis();
+                    mdb.insert(path+s);
+                    Log.v(TAG, "temps "+(System.currentTimeMillis()-t));
+                }
+                catch(SQLiteException sqle)
+                {
+                    Log.v(TAG, "sql" + sqle.toString());
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Log.v(TAG, ex.toString());
+        }
 
     }
 
     private void fillPlayList()
     {
-	try
-	{
-	    Cursor c = mdb.rawQuery("SELECT ID FROM SONG",null);
-	    int songID [] = new int[c.count()];
-	    int pos = 0;
-	    while(c.next()){
-		songID[pos++] = c.getInt(0); //there is just one column	    	
-	    }
-	    mdb.insertPlaylist(songID);
-	}
-	catch(Exception ex)
-	{
-	    Log.v(TAG, ex.toString());
-	}
+        try
+        {
+            Cursor c = mdb.rawQuery("SELECT ID FROM SONG",null);
+            int songID [] = new int[c.count()];
+            int pos = 0;
+            while(c.next())
+            {
+                songID[pos++] = c.getInt(0); //there is just one column	    	
+            }
+            mdb.insertPlaylist(songID);
+        }
+        catch(Exception ex)
+        {
+            Log.v(TAG, ex.toString());
+        }
     }
 
 }
